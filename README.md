06127936d4a75d00197875a17ae83ed38ca1da43
qiray <zotovyaa@mail.ru>
2019-10-01 19:01:59 +0300

    Change author script fix

--- README.md
+++ README.md
@@ -6,4 +6,4 @@ Some useful scripts:
 
 1. [Pull script](git_pull/pull.sh) - update multiple repositories from list.
 
-2. [Change author](git_change_author/change_author.sh) - fix all wrong author names and emails for all branches and tags.
+2. [Change author](git_change_author/change_author.sh) - fix all wrong author names and emails for all branches and tags. **WARNING! These changes are dangerous because they change git history!**
--- git_change_author/change_author.sh
+++ git_change_author/change_author.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 
 # Script from https://stackoverflow.com/a/750182 for fixing all wrong author names and emails for all branches and tags
+# Run this script from folder with repository to change.
+# WARNING! These changes are dangerous because they change git history!
 # $1 - wrong email
 # $2 - correct name
 # $3 - correct email
 
-git filter-branch --env-filter '
 OLD_EMAIL="$1"
 CORRECT_NAME="$2"
 CORRECT_EMAIL="$3"
-if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+
+git filter-branch --env-filter "
+if [ \$GIT_COMMITTER_EMAIL = $OLD_EMAIL ]
 then
-    export GIT_COMMITTER_NAME="$CORRECT_NAME"
-    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+    export GIT_COMMITTER_NAME=$CORRECT_NAME
+    export GIT_COMMITTER_EMAIL=$CORRECT_EMAIL
 fi
-if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+if [ \$GIT_AUTHOR_EMAIL = $OLD_EMAIL ]
 then
-    export GIT_AUTHOR_NAME="$CORRECT_NAME"
-    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+    export GIT_AUTHOR_NAME=$CORRECT_NAME
+    export GIT_AUTHOR_EMAIL=$CORRECT_EMAIL
 fi
-' --tag-name-filter cat -- --branches --tags
+" --tag-name-filter cat -- --branches --tags
